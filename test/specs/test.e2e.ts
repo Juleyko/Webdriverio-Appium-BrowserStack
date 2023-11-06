27b5fc7ed7db5f956627e31d7256f7013002e9f7
Julia Boreiko <juleyko7@gmail.com>
2023-11-06 02:00:36 +0200

    change in code

--- test/specs/test.e2e.ts
+++ test/specs/test.e2e.ts
@@ -1,6 +1,5 @@
 import { expect, browser } from "@wdio/globals";
 import main from "../pageobjects/main.page.js";
-import event from "../pageobjects/event.page.js";
 
 const addEventFiedResourceld =
   "com.samsung.android.calendar:id/quick_add_button";
@@ -17,55 +16,32 @@ describe("My calendar application", () => {
     // await main.clickOnTurnOnButton();
     await browser.pause(5000);
     const selector = await main.getResourceIdSelector(addEventFiedResourceld);
-    // expect(await main.isAddEventFieldDisplayed(selector)).toBe(true);
+    expect(await main.isAddEventFieldDisplayed(selector)).toBe(true);
   });
 
   it(" [case 2]should be visible month", async () => {
-    // await browser.pause(7000);
     const selector = await main.getResourceIdSelector(monthString);
-    // expect(await main.isAddEventFieldDisplayed(selector)).toBe(true);
+    expect(await main.isAddEventFieldDisplayed(selector)).toBe(true);
   });
 
   it(" [case 3]should be visible menu", async () => {
     await browser.pause(5000);
     const selector = await main.getResourceIdSelector(menuButton);
-    // expect(await main.isAddEventFieldDisplayed(selector)).toBe(true);
+    expect(await main.isAddEventFieldDisplayed(selector)).toBe(true);
   });
 
   it(" [case 4]should be visible serch icon", async () => {
     const selector = await main.getResourceIdSelector(serchIcon);
-    // expect(await main.isAddEventFieldDisplayed(selector)).toBe(true);
+    expect(await main.isAddEventFieldDisplayed(selector)).toBe(true);
   });
 
   it(" [case 5]should add event", async () => {
     await browser.pause(5000);
-    // await main.clickOnAddEventButton();
     const selector = await main.getResourceIdSelector(addEventButton);
     await main.clickElement(selector);
-    // await main.fillEventFieldName();
     const select = await main.getResourceIdSelector(titleEventName);
     await main.clickElement(select);
     await main.setElementInputValue(select, "Win");
-    await browser.pause(5000);
-    // await main.clickOnSaveEventButton();
-    // const selec = await main.getResourceIdSelector(saveButton);
     await main.clickElement(saveButton);
-    await browser.pause(5000);
   });
-
-  // it(" [case 4]should open event", async () => {
-  //   await browser.pause(5000);
-  //   await main.clickOnEventDay();
-  //   await event.clickOnEvent4thDay();
-  //   await event.clickOnGoUpButton();
-  //   await event.clickOnJustClick();
-  //   await browser.pause(5000);
-  // });
-  //   it(" [case 5]should delete event", async () => {
-  //     await browser.pause(5000);
-  //     await main.clickOnEventDay();
-  //     await event.clickOnEvent4thDay();
-  //     await event.clickOnDeleteButton();
-  //     await event.clickOnBusketButton();
-  //     await browser.pause(5000);
 });
